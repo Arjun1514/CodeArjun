b4ec2d820aec6dbfe03e4b370f895402873c9058
Arjun1514 <120532881+Arjun1514@users.noreply.github.com>
2023-09-22 14:01:03 +1000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+const display  = document.querySelector(".display");
+const buttons = document.querySelectorAll("button");
+const specialChars = ["%", "*", "/", "-", "+", "="];
+let output = "";
+
+
+//define calculate function to calculate based on button clicked
+
+const calculate = (btnValue) =>{
+  if (btnValue === "=" && output !== ""){
+    output = eval(output.replace("%", "/100"));
+  } else if (btnValue === "AC"){
+    output = "";
+  } else if (btnValue === "DEL"){
+    output =output.toString().slice(0, -1);
+  }else{
+    if (output === "" && specialChars.includes(btnValue)) return;
+      output += btnValue;
+  }
+  display.value = output;
+};
+
+
+
+//adding eventlistner to calculte
+
+buttons.forEach((button)=> {
+  button.addEventListener("click", (e) => calculate(e.target.dataset.value));
+});
